e7d1dfe05c1a1c0a80f39404408a547f821dfc8b
Haroun Mamalo <mamalostar12311@gmail.com>
2025-08-27 13:54:46 +0800

    Implemented Region Filter and Use Case if Both Search and Region is True

--- src/components/CountryList.vue
+++ src/components/CountryList.vue
@@ -5,7 +5,9 @@ import Countries from "@/data.json";
 import axios from "axios";
 import Country from "./Country.vue";
 
-const countrySearch = ref('');
+const countrySearch = ref("");
+const region = ref("");
+
 const props = defineProps({
   viewable: Number,
 });
@@ -15,28 +17,89 @@ const state = reactive({
 state.countries = Countries;
 
 const toggleSearch = computed(() => countrySearch.value.trim().length > 0);
+const toggleRegion = computed(() => {
+  if (region.value.trim().length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+});
 
 const filteredCountry = computed(() => {
   const search = countrySearch.value.trim().toLowerCase();
-  if(!search) return [];
+  if (!search) return [];
 
-  return state.countries.filter(country =>
+  return state.countries.filter((country) =>
     country.name.toLowerCase().includes(search)
   );
-})
+});
 
+const filteredRegion = computed(() => {
+  if (toggleRegion) {
+    return state.countries.filter((country) =>
+      country.region.includes(region.value)
+    );
+  } else {
+    return [];
+  }
+});
 
+const filteredRegionAndSearch = computed(() => {
+  const search = countrySearch.value.trim().toLowerCase();
+  if (toggleRegion && search) {
+    const regionFilter = state.countries.filter((country) =>
+      country.region.includes(region.value)
+    );
+    const searchFilter = regionFilter.filter((country) =>
+      country.name.toLowerCase().includes(search)
+    );
+    return searchFilter;
+  } else {
+    return [];
+  }
+});
+
+const clearRegionFilter = () => {
+  region.value = "";
+};
 </script>
 
 <template>
   <div class="main min-w-full flex justify-center py-5">
     <div class="container w-[80%] mt-10">
-      <div class="filters">
-        <input type="text" v-model="countrySearch" />
+      <div class="filters flex justify-between mb-5">
+        <input
+          type="text"
+          v-model="countrySearch"
+          class="p-2 drop-shadow-md rounded-md"
+        />
+        <div class="drop-down-filter drop-shadow-md rounded-md">
+          <button
+            v-if="toggleRegion"
+            class="p-2 drop-shadow-md rounded-md bg-red-500 mx-2 text-white"
+            @click="clearRegionFilter"
+          >
+            Clear Filter
+          </button>
+          <select
+            name="region"
+            id="region"
+            v-model="region"
+            class="p-2 rounded-md"
+          >
+            <option value="" disabled selected hidden>Filter by Region</option>
+            <option value="Africa">Africa</option>
+            <option value="Americas">Americas</option>
+            <option value="Asia">Asia</option>
+            <option value="Europe">Europe</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </div>
       </div>
       <div class="main-list grid grid-cols-4 gap-20">
+        <!-- Origin Countries without Search and Region Filter -->
         <Country
-          v-if="!toggleSearch"
+          v-if="!toggleSearch && !toggleRegion"
           v-for="(country, index) in state.countries.slice(
             0,
             viewable || state.countries.length
@@ -45,17 +108,28 @@ const filteredCountry = computed(() => {
           :index="index"
           :key="country.name"
         />
-
-        <div
-          v-else-if="toggleSearch"
-          v-for="(country, index) in filteredCountry"
-        >
-        <Country 
-          :country="country"
+        <!-- Countries with Search Filter -->
+        <Country
+          v-else-if="toggleSearch && !toggleRegion"
+          v-for="(searchCountry, index) in filteredCountry"
+          :country="searchCountry"
           :index="index"
-          :key="country.name"
+          :key="searchCountry.name"
         />
-        </div>
+        <!-- Countries with Region Filter -->
+        <Country
+          v-else-if="toggleRegion && !toggleSearch"
+          v-for="regionCountry in filteredRegion"
+          :country="regionCountry"
+          :key="regionCountry.name"
+        />
+        <!-- Both Filter Activated -->
+        <Country
+          v-else-if="toggleRegion && toggleSearch"
+          v-for="countrySearchAndRegion in filteredRegionAndSearch"
+          :country="countrySearchAndRegion"
+          :key="countrySearchAndRegion.name"
+        />
       </div>
     </div>
   </div>
